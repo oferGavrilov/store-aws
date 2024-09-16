b54e7e33c35cb7ac186c27ea153bbff95f78b8bc
Ofer <ofergavri@gmail.com>
2024-09-16 23:42:03 +0300

    server modifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ app.use('/teams', teamRoutes);
 
 
 // Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 3000;
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
